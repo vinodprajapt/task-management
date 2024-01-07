b9af7f1a540afc15d19256c79f31db92648df835
Vinod P <vinodprajapti02005@gmail.com>
2024-01-07 16:45:20 +0530

    Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,39 +3,69 @@ import PropTypes from "prop-types";
 import "./style.css";
 import { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Card = (props) => {
   const { label, type } = props;
   const [show, setShow] = useState(false);
+  const [description, setDiscription] = useState("");
+  const [title, setTitle] = useState("");
+
+  const [tasks, setTasks] = useState([]);
+
+  // object uske andar title or description add
+  const todoTasks = !!tasks.length && tasks.filter((d) => d.type === "todo");
+  const inProgressTasks = tasks?.filter((d) => d.type === "inProgress") ?? [];
+  const doneTasks = tasks?.filter((d) => d.type === "done") ?? [];
+  
+  console.log("tasks", tasks);
   return (
     <>
-    <div className="card">
-      <div className="card-header">
-        <h2>{label}</h2>
-        {type === "todo" &&(
-          <Button className="bg-success border-secondary" variant="primary" onClick={()=>setShow(!show)}>
-            Add new Task{" "}
-          </Button>
-        )}
+      <div className="card">
+        <div className="card-header">
+          <h2>{label}</h2>
+          {type === "todo" && (
+            <Button
+              className="bg-success border-secondary"
+              variant="primary"
+              onClick={() => setShow(!show)}
+            >
+              Add new Task{" "}
+            </Button>
+          )}
+        </div>
+        {!!todoTasks.length &&
+          todoTasks.map((todoTask, index) => (
+            <div className="task" key={index}>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <span className="title">{todoTask.title}</span>
+                <Button>Move to InProgress </Button>
+              </div>
+              <p className="description">{todoTask.description}</p>
+            </div>
+          ))}
+        {!!inProgressTasks.length &&
+          inProgressTasks.map((inProgress, index) => (
+            <div className="task" key={index}>
+              <span className="title">{inProgress.title}</span>
+              <Button>Move to TODO </Button>
+              <Button>Move to Done</Button>
+              <p className="description">{inProgress.description}</p>
+            </div>
+          ))}
+
+        {!!doneTasks.length &&
+          doneTasks.map((doneTask, index) => (
+            <div className="task" key={index}>
+              <span className="title">{doneTask.title}</span>
+              <p className="description">{doneTask.description}</p>
+            </div>
+          ))}
       </div>
-      <div className="task">
-        <span className="title">TODO</span>
-        <p className="description">this is my first task</p>
-      </div>
-      <div className="task">
-        <span className="title">In Progress</span>
-        <p className="description">this is my first task</p>
-      </div>
-      <div className="task">
-        <span className="title">Done</span>
-        <p className="description">this is my first task</p>
-      </div>
-    </div>
-    
+
       {/* Modal  */}
       <Modal show={show} onHide={() => setShow(!show)}>
-        <Modal.Header className="bg-secondary" closeButton>
+        <Modal.Header className="bg-primary" closeButton>
           <Modal.Title>Heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>
@@ -45,23 +75,49 @@ const Card = (props) => {
               <Form.Control
                 type="email"
                 placeholder="Enter task Title"
-                autoFocus 
+                value={title}
+                onChange={(event) => {
+                  setTitle(event.target.value);
+                }}
+                autoFocus
               />
             </Form.Group>
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
             >
-              <Form.Label>Task Discription</Form.Label>
-              <Form.Control as="textarea" rows={3} placeholder="Enter task Discription" />
+              <Form.Label>Task Description</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder="Enter task Discription"
+                value={description}
+                onChange={(event) => {
+                  setDiscription(event.target.value);
+                }}
+              />
             </Form.Group>
           </Form>
         </Modal.Body>
-        <Modal.Footer className="bg-secondary">
-        <Button  className="border-black bg-dark"variant="secondary" onClick={() => setShow(!show)}>
+        <Modal.Footer>
+          <Button
+            className="border-black bg-dark"
+            variant="secondary"
+            onClick={() => setShow(!show)}
+          >
             Close
           </Button>
-          <Button variant="primary" onClick={() => setShow(!show)}>
+          <Button
+            variant="primary"
+            onClick={function () {
+              setShow(!show);
+              const existingData = [...tasks];
+              existingData.push({ title, description, type: "todo" });
+              setTasks(existingData);
+              setTitle('');
+              setDiscription('');
+            }}
+          >
             Save
           </Button>
         </Modal.Footer>
